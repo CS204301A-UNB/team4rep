1c20bffa74ca6504b1766f5c88d6bc2405deb88d
dsteen338 <Dylan.Steen@unb.ca>
2015-12-01 17:27:49 -0400

    here is mine

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -2,6 +2,9 @@
 public class InfoApp {
 	public static void main(String [ ] args)
 	{
+		InfoPrintSteen print1 = new InfoPrintSteen();
 		System.out.println("Team 4 member list: ");
+		
+		print1.printSteen();
 	}
 }
