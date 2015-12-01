2b1de23e5bc0eeb5fc74903245d26f7f6e7ce708
MackenzieJones <MackenzieJones@unb.ca>
2015-12-01 17:30:48 -0400

    Wow

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -3,5 +3,7 @@ public class InfoApp {
 	public static void main(String [ ] args)
 	{
 		System.out.println("Team 4 member list: ");
+		InfoPrintJones wow = new InfoPrintJones();
+		wow.printJones();
 	}
 }
